464dc4f40866a1ba26b7dde7f086cbb3daf98d61
William Tsou <william905cm@gmail.com>
2026-01-23 20:36:30 +0800

    Split header into component and use link

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,9 +1,11 @@
+import { Header } from '../components/header';
 import './OrdersPage.css';
 
 export function OrdersPage() {
   return (
     <>
       <title>Orders</title>
+      <Header />
       <div className="header">
         <div className="left-section">
           <a href="/" className="header-link">
