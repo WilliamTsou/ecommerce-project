e541dfacedb28231851a21ee7035bb7c300baf60
William Tsou <william905cm@gmail.com>
2026-01-30 15:37:57 +0800

    Seperate into smaller components

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import { Routes, Route } from 'react-router';
 import { useState, useEffect } from 'react';
-import { Homepage } from './pages/HomePage';
+import { Homepage } from './pages/home/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
-import { OrdersPage } from './pages/OrdersPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
 import { NotFoundPage } from "./pages/NotFoundPage";
 import './App.css';
