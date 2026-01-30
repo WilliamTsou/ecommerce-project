b9b2b0c3d2a0b99176aca685f7a0306c7ff2c2e0
William Tsou <william905cm@gmail.com>
2026-01-30 15:48:15 +0800

    Convert home and checkout into async await

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -11,12 +11,12 @@ export function Homepage({ cart }) {
   
 
   useEffect(() => {
-    axios.get("/api/products").then((response) => {
+    const getHomeData = async () => {
+      const response = await axios.get("/api/products");
       setProducts(response.data);
-    });
+    };
 
-    
-
+    getHomeData();
   }, []);
 
   return (
